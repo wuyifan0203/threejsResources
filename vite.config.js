053da1daeeacc4bcc22a7bad2e9d8ba1d0193301
wuyifan0203 <1208097313@qq.com>
2024-12-27 13:06:05 +0800

    add index.html

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: wuyifan0203 1208097313@qq.com
+ * @Date: 2024-12-27 11:27:53
+ * @LastEditors: wuyifan0203 1208097313@qq.com
+ * @LastEditTime: 2024-12-27 11:29:49
+ * @FilePath: \threejsResources\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+import { defineConfig } from 'vite';
+export default defineConfig({
+    root:'./',
+    base:'/threejsResources/',
+    server:{
+        port:6700
+    },
+})
